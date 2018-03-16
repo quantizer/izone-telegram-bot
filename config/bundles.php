7a05ce2c6375689db91962286b9854053f03e640
Ivan Diadin <jlamep@gmail.com>
2018-03-16 16:10:05 +0200

    Deploy tool and telegram API wrapper installed.

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['dev' => true, 'test' => true],
 ];
